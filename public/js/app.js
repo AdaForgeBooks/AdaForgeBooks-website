cb7e9d54d270afea17861488f7f3971688818051
Todd Thorne <ada-forge@proton.me>
2026-08-02 17:40:04 -0500

    Build Ada Forge Books homepage

---
+++ public/js/app.js
@@ -0,0 +1,21 @@
+const menuButton = document.querySelector(".menu-button");
+const navigation = document.querySelector(".main-navigation");
+const navigationLinks = document.querySelectorAll(".main-navigation a");
+
+if (menuButton && navigation) {
+    menuButton.addEventListener("click", () => {
+        const menuIsOpen = navigation.classList.toggle("is-open");
+
+        menuButton.setAttribute(
+            "aria-expanded",
+            menuIsOpen.toString()
+        );
+    });
+}
+
+navigationLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        navigation.classList.remove("is-open");
+        menuButton?.setAttribute("aria-expanded", "false");
+    });
+});
