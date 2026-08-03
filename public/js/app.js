176de2051e7ff96afe4b559c668858f17a388aaa
Todd Thorne <ada-forge@proton.me>
2026-08-03 08:25:22 -0500

    Amazon button added

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,16 @@
 const menuButton = document.querySelector(".menu-button");
 const navigation = document.querySelector(".main-navigation");
-const navigationLinks = document.querySelectorAll(".main-navigation a");
 
 if (menuButton && navigation) {
     menuButton.addEventListener("click", () => {
-        const menuIsOpen = navigation.classList.toggle("is-open");
+        const open = navigation.classList.toggle("is-open");
+        menuButton.setAttribute("aria-expanded", String(open));
+    });
 
-        menuButton.setAttribute(
-            "aria-expanded",
-            menuIsOpen.toString()
-        );
+    navigation.querySelectorAll("a").forEach((link) => {
+        link.addEventListener("click", () => {
+            navigation.classList.remove("is-open");
+            menuButton.setAttribute("aria-expanded", "false");
+        });
     });
 }
-
-navigationLinks.forEach((link) => {
-    link.addEventListener("click", () => {
-        navigation.classList.remove("is-open");
-        menuButton?.setAttribute("aria-expanded", "false");
-    });
-});
